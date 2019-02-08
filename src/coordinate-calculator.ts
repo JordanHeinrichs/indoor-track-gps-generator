0d95c0d97130f5ad5fec212bb97fdf1e2945099e
Jordan Heinrichs <mail@jordanheinrichs.com>
2019-02-07 23:37:10 -0700

    Fix angle calculations

--- src/coordinate-calculator.ts
+++ src/coordinate-calculator.ts
@@ -10,7 +10,7 @@ export class CoordinateCalculator {
     private readonly firstLegPercent: number;
     private readonly secondLegPercent: number;
     private readonly thirdLegPercent: number;
-    private readonly forthLegPercent: number;
+    private readonly fourthLegPercent: number;
     private readonly centerCoordinate: Coordinate;
 
     constructor(private config: Config) {
@@ -24,36 +24,37 @@ export class CoordinateCalculator {
         this.firstLegPercent = this.sideLength / 2 / config.trackLength;
         this.secondLegPercent = (Math.PI * this.radius + this.sideLength / 2) / config.trackLength;
         this.thirdLegPercent = (Math.PI * this.radius + 3 * this.sideLength / 2) / config.trackLength;
-        this.forthLegPercent = (2 * Math.PI * this.radius + 3 * this.sideLength / 2) / config.trackLength;
+        this.fourthLegPercent = (2 * Math.PI * this.radius + 3 * this.sideLength / 2) / config.trackLength;
     }
 
     public getCoordinateForPercent(percent: number): Coordinate {
+        const distanceTravelled = percent * this.trackLength;
+
         let vectorToTrackLocation: Vector;
         if (percent < this.firstLegPercent) {
             vectorToTrackLocation = Vector.createFromPolar(this.radius, this.offset).add(
-                Vector.createFromPolar(percent * this.sideLength / 2, this.offset + Math.PI / 2));
+                Vector.createFromPolar(distanceTravelled, this.offset + Math.PI / 2));
         } else if (percent < this.secondLegPercent) {
             vectorToTrackLocation = Vector.createFromPolar(this.sideLength / 2, this.offset + Math.PI / 2).add(
                 Vector.createFromPolar(this.radius,
-                    (percent * this.trackLength - this.sideLength / 2) / this.radius + this.offset));
+                    (distanceTravelled - this.sideLength / 2) / this.radius + this.offset));
         } else if (percent < this.thirdLegPercent) {
             vectorToTrackLocation = Vector.createFromPolar(this.sideLength / 2, this.offset + Math.PI / 2).add(
                 Vector.createFromPolar(this.radius, Math.PI + this.offset),
-            ).add(Vector.createFromPolar(percent * this.trackLength - Math.PI * this.radius - this.sideLength / 2,
+            ).add(Vector.createFromPolar(distanceTravelled - Math.PI * this.radius - this.sideLength / 2,
                 3 * Math.PI / 2 + this.offset));
-        } else if (percent < this.forthLegPercent) {
+        } else if (percent < this.fourthLegPercent) {
             vectorToTrackLocation = Vector.createFromPolar(this.sideLength / 2, this.offset + 3 * Math.PI / 2).add(
                 Vector.createFromPolar(this.radius,
-                    (percent * this.trackLength - 3 / 2 * this.sideLength) / this.radius + this.offset),
+                    (distanceTravelled - 3 / 2 * this.sideLength) / this.radius + this.offset),
             );
         } else {
             vectorToTrackLocation = Vector.createFromPolar(this.sideLength / 2, this.offset + 3 * Math.PI / 2).add(
                 Vector.createFromPolar(this.radius, this.offset)).add(
                     Vector.createFromPolar(
-                        percent * this.trackLength - 3 / 2 * this.sideLength - 2 * Math.PI * this.radius,
+                        distanceTravelled - 3 / 2 * this.sideLength - 2 * Math.PI * this.radius,
                         this.offset + Math.PI / 2),
                 );
-
         }
 
         return this.centerCoordinate.add(vectorToTrackLocation.x, vectorToTrackLocation.y, this.config);
